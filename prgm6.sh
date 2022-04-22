5ca6cd40d14b5225d094bed8493fc5048fff9d1d
Deepakajithmm <mailofdeepakajith@gmail.com>
2022-04-22 17:52:13 +0530

    hg

---
+++ prgm6.sh
@@ -0,0 +1,35 @@
+echo "enter 3 numbers"
+read a b c
+if [ $a -gt $b ]
+then
+if [ $a -gt $c ]
+then 
+echo "$a is largest"
+fi
+elif [ $b -gt $c ]
+then
+if [ $b -ge $a ]
+then 
+echo " largest is $b"
+fi 
+else
+echo " largest is $c"
+fi
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
